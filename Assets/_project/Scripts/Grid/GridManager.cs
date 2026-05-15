fbb4c4df4cd842f0838d4d30d1def7cf8efbc49b
YUMINJUN <71377165+MINJUN17@users.noreply.github.com>
2026-05-16 01:11:38 +0900

    Gridmanager 구현
    GridManager, GridCell, GridViualizer 스크립트 작성

--- Assets/_project/Scripts/Grid/GridManager.cs
+++ Assets/_project/Scripts/Grid/GridManager.cs
@@ -31,6 +31,24 @@ public class GridManager : MonoBehaviour
     }
     public GridCell GetCell(int x, int y)
     {
+        if(!IsInBounds(x, y)) 
+        {
+            return null;
+        }
         return _cells[x, y];
+    }
+    public bool IsInBounds(int x, int y)
+    {
+        return x >= 0 && x < _gridWidth && y >= 0 && y < _gridHeight;
+    }
+    public Vector3 CellToWorld(int x, int y) // 가구를 셀에 놓을때 활용
+    {
+        return new Vector3(x + 0.5f, y + 0.5f, 0);
+    }
+    public Vector2Int WorldToCell(Vector3 worldPos)
+    {
+        int x = Mathf.FloorToInt(worldPos.x);
+        int y = Mathf.FloorToInt(worldPos.y);
+        return new Vector2Int(x, y);
     }
 }
---
+++ Assets/_project/Scripts/Grid/GridVisualizer.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class GridVisualizer : MonoBehaviour
+{
+    public GridManager gridManager;
+    public Color activeColor = new Color(0f, 1f, 0f, 0.2f);
+    public Color occupiedColor= new Color(0f, 0f, 1f, 0.3f);
+    public Color inActiveColor= new Color(1f, 0f, 0f, 0.1f);
+
+    private void OnDrawGizmos()
+    {
+        if(gridManager == null) return;
+        if(!Application.isPlaying) return;
+
+        for(int x = 0; x < gridManager.GridWidth; x++)
+        {
+            for(int y = 0; y < gridManager.GridHeight; y++)
+            {
+                GridCell cell = gridManager.GetCell(x, y);
+                if(cell == null) return;
+                if (cell.isOccupied)
+                {
+                    Gizmos.color = occupiedColor;
+                }
+                else if (cell.isActive)
+                {
+                    Gizmos.color = activeColor;
+                }
+                else
+                {
+                    Gizmos.color = inActiveColor;
+                }
+                Vector3 center = gridManager.CellToWorld(x, y);
+                Gizmos.DrawCube(center, new Vector3(0.95f, 0.95f, 0.01f));
+            }
+        }
+    }
+}
